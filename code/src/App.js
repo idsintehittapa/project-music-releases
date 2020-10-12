7c868a98690838fcb9e1b9c4ee14abd795465c97
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-12 16:46:29 +0200

    Refactored code.

--- code/src/App.js
+++ code/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
-//import data from './data.json'
+import data from './data.json'
 import MusicHeader from './MusicHeader'
-import MusicList from './MusicList'
+import MusicElement from './MusicElement'
 
-//console.log(data)
+console.log(data)
 
 //This is the file where we import all our components!
-
 export const App = () => {
   return (
-    //this is JSX (our output):
-    <>
+    <div>
       <MusicHeader />
-      <MusicList />
-    </>
+    
+    {data.albums.items.map((item) => (
+      <MusicElement 
+      key={item.id} 
+      image={item.images[1].url}
+      album={item.name}
+      artist={item.artists[0].name}
+      />
+    ))}
+    </div>
   )
 }
+
--- code/src/MusicElement.js
+++ code/src/MusicElement.js
@@ -1,35 +1,21 @@
 import React from 'react'
-import data from './data.json'
-import MusicAlbumcover from './MusicAlbumcover'
-import MusicText from './MusicText'
 
-console.log(data)
 //Component for each album-item
-//Should we do a map-function here? 8:04 in video.
 const MusicElement = (props) => {
   return (
-    <>
-      <div>
-        {data.albums.items.map((item) => (
-          <MusicAlbumcover
-          key={item.id}  
-          //Kolla upp key!
-          image={item.images[1].url}
-          /> 
-        ))}
-        
+    <section className="album-wrapper">
+      <div className="album-cover">
+        <img src={props.image} alt="album cover" />
       </div>
-      <div>
-        {data.albums.items.map((item) => (
-          <MusicText 
-          key={item.id}
-          album={item.name}
-          artist={item.artists[0].name}
-          />
-        ))}
-        
+
+      <div className="album-name">
+        <p> {props.album}</p>
       </div>
-    </>
+
+      <div className="artist-name">
+        <p> {props.artist}</p>
+      </div>
+    </section>
   )
 }
 
