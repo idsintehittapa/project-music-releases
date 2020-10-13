0e3ab247be0226e77e696c1c1238748d4d76928f
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-13 20:31:01 +0200

    Styled icons and overlay. Added hover-effect.

--- code/src/MusicElement.js
+++ code/src/MusicElement.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { ReactComponent as Heart } from "./icons/heart.svg"
+import { ReactComponent as Play } from "./icons/play.svg"
+import { ReactComponent as Dots } from "./icons/dots.svg"
 
 //Component for each album-item
 const MusicElement = (props) => {
@@ -6,6 +9,11 @@ const MusicElement = (props) => {
     <div className="album">
       <div className="album-cover">
         <img src={props.image} alt="album cover" />
+        <div className="overlay"> 
+          <div className="heart-icon"><Heart /></div>
+          <div className="play-icon"><Play /></div>
+          <div className="dots-icon"><Dots /></div>
+        </div>
       </div>
 
       <div className="album-name">
