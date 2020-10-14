fb63078b54ccd61fe6b9809ea2058e73786121af
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-14 19:13:22 +0200

    Updates

--- code/src/ArtistsLinks.js
+++ code/src/ArtistsLinks.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ArtistsLinks = props => {
     return (
-        <a className="artistInfo" href={props.item.external_urls.spotify} target="_blank" rel="noopener noreferrer">{props.item.name}</a>
+        <a 
+          className="artistInfo" 
+          href={props.item.external_urls.spotify} 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          {props.item.name}
+        </a>
 
     )
 }
