ebbf0ce076bee0d824af6afff14baa0a2321c5a9
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-16 14:42:29 +0200

    Done styling. Hover-effects.

--- code/src/ArtistsLinks.js
+++ code/src/ArtistsLinks.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const ArtistsLinks = props => {
-    return (
-        <a
-            className="artist-info"
-            href={props.item.external_urls.spotify}
-            rel="noopener noreferrer"
-        >
-            {props.item.name}
-        </a>
-    )
+  return (
+    <a
+      className="artist-info"
+      href={props.item.external_urls.spotify}
+      rel="noopener noreferrer"
+    >
+      {props.item.name}
+    </a>
+  )
 }
 export default ArtistsLinks
--- code/src/Footer.js
+++ code/src/Footer.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-                <p className="footer-text"> This is a school project for the Technigo Bootcamp 2020 made by Johanna Blom & Hanne Skeppe.
-                </p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <p className="footer-text">This is a project made for the Technigo front-end developer Bootcamp 2020 made by Hanne Skeppe and Johanna Blom.</p>
+    </footer>
+  )
 };
 
 export default Footer;
